27c1037495cb104b0dc9f7517492259da1928e49
Roman <roman.makuha@gmail.com>
2019-09-15 17:47:38 +0300

    Updates

---
+++ precache-manifest.bb534d4a5bf60be2913c51bca118b603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d582ef9648e51364e6478a35eccdb3b6",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "fa9ff43408f3e98379f9",
+    "url": "/ToDo-App/static/css/main.72494181.chunk.css"
+  },
+  {
+    "revision": "89c1dee689022a7e752e",
+    "url": "/ToDo-App/static/js/2.144fec58.chunk.js"
+  },
+  {
+    "revision": "fa9ff43408f3e98379f9",
+    "url": "/ToDo-App/static/js/main.88d308fb.chunk.js"
+  },
+  {
+    "revision": "9957314125854f35cd44",
+    "url": "/ToDo-App/static/js/runtime~main.1ef025cf.js"
+  }
+]);
